2edb400c2f667540705c2b38509f202377b775f1
miciav <michele.ciavotta@gmail.com>
2016-02-29 12:30:55 +0100

    Changes in shared settings

--- src/main/java/it/polimi/diceH2020/SPACE4Cloud/shared/settings/Settings.java
+++ src/main/java/it/polimi/diceH2020/SPACE4Cloud/shared/settings/Settings.java
@@ -16,45 +16,13 @@
  */
 package it.polimi.diceH2020.SPACE4Cloud.shared.settings;
 
+import lombok.Data;
+
+@Data
 public class Settings {
 
-	public Settings(double accuracy, int cycles) {
-		super();
-		this.accuracy = accuracy;
-		this.cycles = cycles;
-	}
-
-	private double accuracy;
-	private int cycles;
-
-	/**
-	 * @return the accuracy
-	 */
-	public double getAccuracy() {
-		return accuracy;
-	}
-
-	/**
-	 * @param accuracy
-	 *            the accuracy to set
-	 */
-	public void setAccuracy(double accuracy) {
-		this.accuracy = accuracy;
-	}
-
-	/**
-	 * @return the cycles
-	 */
-	public int getCycles() {
-		return cycles;
-	}
-
-	/**
-	 * @param cycles
-	 *            the cycles to set
-	 */
-	public void setCycles(int cycles) {
-		this.cycles = cycles;
-	}
+	private Double accuracy = 30d;
+	private Integer simDuration = 180;
+	private Solver solver = Solver.QN;
 
 }
---
+++ src/main/java/it/polimi/diceH2020/SPACE4Cloud/shared/settings/Solver.java
@@ -0,0 +1,5 @@
+package it.polimi.diceH2020.SPACE4Cloud.shared.settings;
+
+public enum Solver {
+	SPN, QN;
+}
