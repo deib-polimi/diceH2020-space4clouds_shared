40a6516eea3b71c0a3b2f3059b0637a332a308a7
jacoporigoli <jackeape@gmail.com>
2016-07-29 11:17:42 +0200

    Cost For VMConfiguration

--- src/main/java/it/polimi/diceH2020/SPACE4Cloud/shared/inputDataMultiProvider/VMConfiguration.java
+++ src/main/java/it/polimi/diceH2020/SPACE4Cloud/shared/inputDataMultiProvider/VMConfiguration.java
@@ -16,6 +16,8 @@
  */
 package it.polimi.diceH2020.SPACE4Cloud.shared.inputDataMultiProvider;
 
+import java.util.Optional;
+
 import lombok.Data;
 
 @Data
@@ -23,6 +25,7 @@ public class VMConfiguration {
 	private double core;
     private double memory;
     private String provider = "inHouse";
+    private Optional<Double> cost = Optional.empty(); //Per node per hour
     
     public boolean validate(){
 		return (core>=0 && memory>=0 && provider.equals("inHouse"));
